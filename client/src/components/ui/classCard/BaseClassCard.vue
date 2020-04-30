1b1b29b5234c60d8ff3111e04e432c62e105a0c7
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-04-30 20:24:13 +0300

    refactor: conditionally show tags around slots

---
+++ client/src/components/ui/classCard/BaseClassCard.vue
@@ -0,0 +1,64 @@
+<template functional>
+  <section class="w-56 rounded shadow space-y-1">
+    <div class="rounded-t w-full h-32 bg-blue-500" />
+
+    <article class="space-y-1 px-2 pb-1">
+      <h1 class="text-lg font-bold">
+        <slot name="title" />
+      </h1>
+
+      <p
+        v-if="!!scopedSlots['start-date']"
+        class="text-blue-900 text-xs"
+      >
+        <slot name="start-date" />
+      </p>
+
+      <p
+        v-if="!!scopedSlots['student-count']"
+        class="text-blue-900 text-xs"
+      >
+        <slot name="student-count" />
+      </p>
+
+      <p
+        v-if="!!scopedSlots.progress"
+        class="text-blue-900 text-xs"
+      >
+        <slot name="progress" />
+      </p>
+
+      <div
+        v-if="props.showButtons"
+        class="flex justify-between"
+      >
+        <button class="btn btn-blue-sec">
+          View
+        </button>
+
+        <button class="btn btn-blue">
+          Manage
+        </button>
+      </div>
+
+      <p
+        v-if="!!scopedSlots.description"
+        class="text-sm h-20 leading-snug overflow-hidden"
+      >
+        <slot name="description" />
+      </p>
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BaseClassCard',
+  props: {
+    showButtons: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
