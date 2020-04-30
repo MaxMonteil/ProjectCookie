0e8118f0080d8f4804ce838772874aabe2c27aad
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-04-30 22:00:52 +0300

    design: fix header to the top of the page

--- client/src/components/ui/TheHeader.vue
+++ client/src/components/ui/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="bg-blue-500 px-32 py-4 w-full flex items-center space-x-8 shadow">
+  <header class="fixed top-0 left-0 bg-blue-500 px-32 py-4 w-full flex items-center space-x-8 shadow">
     <h1 class="text-2xl text-white font-bold">
       ProjectCookie
     </h1>
@@ -7,6 +7,7 @@
     <SearchBar
       v-model="search"
       label="search"
+      :with-opacity="true"
     />
 
     <div class="p-4 w-8 h-8 bg-blue-900 rounded-full" />
