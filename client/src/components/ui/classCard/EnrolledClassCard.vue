12d1355adbf7c139d17c1045c5d18a8435ec7a47
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-04-30 21:59:13 +0300

    design: create composed component card for enrolled courses

---
+++ client/src/components/ui/classCard/EnrolledClassCard.vue
@@ -0,0 +1,30 @@
+<template functional>
+  <component :is="injections.components.BaseClassCard">
+    <template #title>
+      {{ props.course.title }}
+    </template>
+
+    <template #progress>
+      Progress: {{ props.course.progress }}%
+    </template>
+  </component>
+</template>
+
+<script>
+export default {
+  name: 'EnrolledClassCard',
+  inject: {
+    components: {
+      default: {
+        BaseClassCard: () => import('@/components/ui/classCard/BaseClassCard'),
+      },
+    },
+  },
+  props: {
+    course: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
