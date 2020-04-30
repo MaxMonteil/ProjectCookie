2fe7e15e056aa111696bb31fb8cedd22b4bd192f
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-04-30 22:00:01 +0300

    design: create layout for users enrolled courses

---
+++ client/src/layouts/EnrolledCoursesLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="flex flex-col">
+    <h1 class="text-3xl leading-none font-semibold mb-6">
+      Enrolled Courses
+    </h1>
+
+    <ClassCardRow
+      card="enrolled"
+      :courses="courses"
+    />
+
+    <a
+      href="#"
+      class="text-blue-900 text-sm underline self-end mt-4"
+    >
+      See all
+    </a>
+  </section>
+</template>
+
+<script>
+import ClassCardRow from '@/components/ui/classCard/ClassCardRow'
+
+export default {
+  name: 'EnrolledCoursesLayout',
+  components: {
+    ClassCardRow,
+  },
+  props: {
+    courses: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
