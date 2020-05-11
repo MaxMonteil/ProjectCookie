eb65a7dcc69875d0ec41c879783a8726c10eb178
msm56 <msm56@mail.aub.edu>
2020-05-11 18:53:55 +0300

    add user field validation

--- server/app/models/Users.php
+++ server/app/models/Users.php
@@ -16,7 +16,20 @@ class Users {
      */
     public static function newUser($user): void {
         // field validation
-
+        PDOException $e;
+        if(!preg_match(^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$,$password)){
+            die(var_dump($e->"This password is weak, please enter another one"));
+        }
+        if(!preg_match(^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$, $Email)){
+            die(var_dump($e->"Invalid email, please enter another one"));
+        }
         App::get('database')->insert(static::$table, $user);
+    }
+    public static function getUser($user) {
+        $columns = ['UserID', 'Name', 'Email', 'Password', 'Verified', 'HashEmail'];
+        return App::get('database')->selectOne(static::$table, $user, $columns);
+    }
+    public static function updatePass($user, $Email){
+        return App::get('database')->update(static::$table, $user, $Email);
     }
 }
