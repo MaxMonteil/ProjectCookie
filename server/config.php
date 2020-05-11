e68d88e7e3f2d31d3c8da9e575ac7d3d5710b4c2
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-05-11 12:02:45 +0300

    build: refer config file to env variables

--- server/config.php
+++ server/config.php
@@ -2,12 +2,16 @@
 
 return [
     'database' => [
-        'name' => '',
-        'username' => '',
-        'password' => '',
-        'connection' => '',
+        'name' => $_ENV['DB_NAME'],
+        'username' => $_ENV['DB_USERNAME'],
+        'password' => $_ENV['DB_PASSWORD'],
+        'connection' => $_ENV['DB_CONNECTION'],
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         ],
+    ],
+    'email' => [
+        'username' => $_ENV['EMAIL_USERNAME'],
+        'password' => $_ENV['EMAIL_PASSWORD'],
     ],
 ];
