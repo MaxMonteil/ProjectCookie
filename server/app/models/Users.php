952a6aa73be6003714a8fe12f0f585e7df999910
Maximilien Monteil <maximilienmonteil@gmail.com>
2020-05-13 23:33:51 +0300

    fix: for client api

--- server/app/models/Users.php
+++ server/app/models/Users.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Core\App;
 
 class Users {
-    protected static $table = 'users';
+    protected static $table = 'Users';
 
     /**
      * Insert a new user into the database
@@ -16,11 +16,11 @@ class Users {
      */
     public static function newUser($user): void {
         // field validation
-        if(!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[!@#$%^&*])[0-9A-Za-z!@#$%^&*]{8,}$/", $user['Password'])){
+        if (!preg_match("/^(?=.*\d)(?=.*[a-z])(?=.*[!@#$%^&*])[0-9A-Za-z!@#$%^&*]{8,}$/", $user['Password'])) {
             throw new \Exception("This password is weak, please enter another one");
         }
         $user['Password'] = password_hash($user['Password'], PASSWORD_BCRYPT);
-        if(!preg_match("/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]{2,})+(\.[a-zA-Z0-9-]{2,})*(\.[a-zA-Z]{2,})$/", $user['Email'])){
+        if (!preg_match("/^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]{2,})+(\.[a-zA-Z0-9-]{2,})*(\.[a-zA-Z]{2,})$/", $user['Email'])) {
             throw new \Exception("Invalid email, please enter another one");
         }
         App::get('database')->insert(static::$table, $user);
@@ -44,7 +44,7 @@ class Users {
      *
      * @return void
      */
-    public static function updatePass($user){
+    public static function updatePass($user) {
         $password_hash = password_hash($user['Password'], PASSWORD_BCRYPT);
         App::get('database')->update(static::$table, ['Password'=> $password_hash], ['Email'=> $user['Email']]); // $user = [Email => "ksjd@gmail.com", password => "something"]
     }
@@ -55,8 +55,7 @@ class Users {
      *
      * @return void
      */
-    public static function verifyUser($user){
+    public static function verifyUser($user) {
         App::get('database')->update(static::$table, ['EmailHash'=> null, 'Verified'=> 1], ['Email'=> $user['Email']]);
     }
-
 }
